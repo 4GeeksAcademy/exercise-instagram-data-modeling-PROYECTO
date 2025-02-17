c6c6982f6c433f4ac6508dda449a35d033e92fa9
Alejandro <alejandrorcc24@gmail.com>
2025-02-17 21:28:58 +0000

    finalizado

--- src/models.py
+++ src/models.py
@@ -1,34 +1,61 @@
-import os
-import sys
-from sqlalchemy.orm import declarative_base, Mapped, mapped_column
-from sqlalchemy import create_engine
-from eralchemy2 import render_er
+from sqlalchemy import Column, Integer, String, enumerate, ForeingnKey
+from sqlalchemy.ext.declarative import declarative_basefrom
+from sqlalchemy.orm import relationship
+
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(nullable=False)
+class User(Base):
+    __tablename__ = 'users'
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id: Mapped[int] = mapped_column(primary_key=True)
-    street_name: Mapped[str]
-    street_number: Mapped[str]
-    post_code: Mapped[str] = mapped_column(nullable=False)
+    id = Column(Integer, primary_key=True)
+    username = Column(String(100))
+    firstname = Column(String(100)) 
+    lastname = Column (String(100)) 
+    email = Column(String(120), nullable=False, unique=True)
 
-    def to_dict(self):
-        return {}
+    User = relationship('user', backref="post", uselist=False)
+    User = relationship('user', backref="follower", uselist=False)
+    User = relationship('user', backref="comment", uselist=False)
 
-## Draw from SQLAlchemy base
-try:
-    result = render_er(Base, 'diagram.png')
-    print("Success! Check the diagram.png file")
-except Exception as e:
-    print("There was a problem genering the diagram")
-    raise e
+
+
+class Post(Base):
+    __tablename__ = 'post'
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeingnKey('users.id'), nullable=False)
+
+    post = relationship('post', backref="comment", uselist=False)
+    post = relationship('post', backref="media", uselist=False)
+
+
+
+
+class Follower(Base):
+    __tablename__ = 'follower'
+
+    user_from_id = Column(Integer, ForeingnKey('users.id'), nullable=False)
+    user_to_id = Column(Integer, ForeingnKey('users.id'), nullable=False)
+
+
+
+
+class Comment(Base):
+    __tablename__ = 'comment'
+
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(100))
+    author_id = Column(Integer, ForeingnKey('post.id'), nullable=False)
+    post_id = Column(Integer, ForeingnKey('post.id'), nullable=False)
+
+
+
+
+class Media(Base):
+    __tablename__ = 'media'
+
+    id = Column(Integer, primary_key=True)
+    type = Column(enumerate(100))
+    url = Column(String(100))
+    post_id = Column(Integer, ForeingnKey('post.id'), nullable=False)
